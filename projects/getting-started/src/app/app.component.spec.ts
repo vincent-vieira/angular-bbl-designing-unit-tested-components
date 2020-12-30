4b3339a4b8611bc2711693ae5a0979b7cd631e48
Vincent Vieira <vincent.vieira@carbon-it.com>
2020-12-30 18:54:09 +0100

    refactoring on app component

--- projects/getting-started/src/app/app.component.spec.ts
+++ projects/getting-started/src/app/app.component.spec.ts
@@ -9,6 +9,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-
 });
--- projects/refactored-solution/src/app/app.component.ts
+++ projects/refactored-solution/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.sass"],
+  selector: 'tic-tac-toe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {}
