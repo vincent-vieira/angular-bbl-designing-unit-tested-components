8782f3939ac5c72eed96ac989407e841378cba82
Vincent Vieira <vincent.vieira@carbon-it.com>
2021-01-04 17:11:03 +0100

    game service without any observables, it is way too cumbersome

--- projects/refactored-solution/src/app/game/game.service.ts
+++ projects/refactored-solution/src/app/game/game.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, combineLatest, Observable, ReplaySubject, zip } from 'rxjs';
-import { map, mergeAll, scan, skipWhile, startWith, windowToggle, withLatestFrom } from 'rxjs/operators';
 import { TicTacToeBoard, TicTacToePlayer } from './game.models';
 
 type TicTacToeGameState = {
@@ -8,42 +6,6 @@ type TicTacToeGameState = {
   currentPlayer: TicTacToePlayer;
 };
 type TicTacToeGameStateHistory = Array<TicTacToeGameState>;
-type TicTacToeGamePlay = { player: TicTacToePlayer, index: number };
-
-const gameStateHistoryBuilder = (accumulator,
-                                 [gamePlay, size, currentStepNumber]: [TicTacToeGamePlay, number, number]): TicTacToeGameStateHistory => {
-  const {player: currentPlayer, index: playIndex} = gamePlay;
-
-  if (accumulator.length === 0) {
-    const initialSquares = Array.from(
-      {length: size * size},
-      () => '' as TicTacToePlayer
-    );
-    const squares = Array.from(
-      {length: size * size},
-      (_, index) => index === playIndex ? currentPlayer : '' as TicTacToePlayer
-    );
-    return [
-      ...accumulator,
-      {currentPlayer, squares: initialSquares},
-      {currentPlayer, squares}
-    ];
-  } else {
-    const lastMoves = [...accumulator.slice(0, currentStepNumber + 1)];
-    const lastMove = lastMoves[lastMoves.length - 1].squares;
-    const squares = lastMove.map((playerOnSquare, index) => {
-      if (index === playIndex && playerOnSquare === '') {
-        return currentPlayer;
-      }
-      return playerOnSquare;
-    });
-
-    return [
-      ...accumulator,
-      {currentPlayer, squares}
-    ];
-  }
-};
 
 const calculateWinner = (squares: TicTacToeBoard): TicTacToePlayer | null => {
   const lines = [
@@ -64,64 +26,78 @@ const calculateWinner = (squares: TicTacToeBoard): TicTacToePlayer | null => {
       squares[a] === squares[b] &&
       squares[a] === squares[c]
     ) {
-      return squares[a];
+      return squares[a] as TicTacToePlayer;
     }
   }
   return null;
 };
 
+const switchPlayer = (currentPlayer: TicTacToePlayer): TicTacToePlayer => {
+  return currentPlayer === 'X' ? 'O' : 'X';
+};
+
 @Injectable()
 export class GameService {
 
-  private readonly currentStepNumber = new BehaviorSubject<number>(0);
-  private readonly currentPlayer = new BehaviorSubject<TicTacToePlayer>('X');
-  private readonly plays = new ReplaySubject<TicTacToeGamePlay>(1);
-  private readonly boardSize = new ReplaySubject<number>(1);
+  private history: TicTacToeGameStateHistory = [];
+  private currentPlayer: TicTacToePlayer;
+  private currentStepNumber: number;
 
-  readonly squares: Observable<TicTacToeBoard>;
-  readonly hasGameStarted: Observable<boolean>;
-  readonly winner: Observable<TicTacToePlayer>;
-  readonly historySize: Observable<number>;
+  get squares(): TicTacToeBoard {
+    return this.history[this.currentStepNumber]?.squares || [];
+  }
 
-  constructor() {
-    const gameMoves = combineLatest([
-      zip(this.plays, this.currentStepNumber),
-      this.boardSize
-    ]).pipe(
-      map(([[gameState, currentStepNumber], size]) => [gameState, size, currentStepNumber] as [TicTacToeGamePlay, number, number])
-    );
+  get nextPlayer(): TicTacToePlayer {
+    return switchPlayer(this.currentPlayer);
+  }
 
-    const currentGameState = gameMoves.pipe(
-      scan(gameStateHistoryBuilder, [] as TicTacToeGameStateHistory)
-    );
+  get winner(): TicTacToePlayer | null {
+    return calculateWinner(this.squares);
+  }
 
-    this.squares = currentGameState.pipe(
-      withLatestFrom(this.currentStepNumber),
-      map(([history, currentStepNumber]) => history[currentStepNumber + 1]?.squares),
-      startWith([])
-    );
-    this.historySize = currentGameState.pipe(map(history => history.length));
-    this.hasGameStarted = this.historySize.pipe(map(size => size > 1));
+  get hasGameStarted(): boolean {
+    return this.historySize > 1;
+  }
 
-    /*this.winner = this.squares.pipe(
-      filter(squares => squares && squares.length > 0),
-      map(squares => calculateWinner(squares)),
-      tap(value => console.log(value))
-    );*/
+  get historySize(): number {
+    return this.history.length;
   }
 
   init(size: number): void {
-    this.boardSize.next(size);
+    this.currentStepNumber = 0;
+    this.currentPlayer = 'X';
+    this.history = [
+      {
+        squares: Array.from(
+          {length: size * size},
+          () => '' as TicTacToePlayer
+        ),
+        currentPlayer: this.currentPlayer
+      }
+    ];
   }
 
   play(squareIndex: number): void {
-    this.plays.next({player: this.currentPlayer.value, index: squareIndex});
-    // FIXME : dont allow overlapping positions and going to next step
-    this.currentPlayer.next(this.currentPlayer.value === 'X' ? 'O' : 'X');
-    this.currentStepNumber.next(this.currentStepNumber.value + 1);
+    if (this.winner) {
+      return;
+    }
+
+    const newSquares = this.squares.map((currentPlayer, index) => {
+      if (squareIndex === index && !currentPlayer) {
+        return this.currentPlayer;
+      }
+      return currentPlayer;
+    });
+
+    this.history = [
+      ...this.history.slice(0, this.currentStepNumber + 1),
+      {squares: newSquares, currentPlayer: this.currentPlayer}
+    ];
+    this.currentPlayer = switchPlayer(this.currentPlayer);
+    this.currentStepNumber += 1;
   }
 
   jumpTo(stepNumber: number): void {
-    this.currentStepNumber.next(stepNumber);
+    this.currentStepNumber = stepNumber;
   }
 }
